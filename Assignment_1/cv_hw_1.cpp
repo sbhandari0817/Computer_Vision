1ce30ec64ee8233c723a70ff8ca45f2fde9d79c1
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-06-24 20:09:41 -0500

    working

--- Assignment_1/cv_hw_1.cpp
+++ Assignment_1/cv_hw_1.cpp
@@ -17,6 +17,9 @@ std::string color[4] = {"EYEDROPER","CROP","PENCIL","PAINT BUCKET"};
 
 int EYEDROPER[3] ={255, 255, 255}; //intializing eye dropper to 255, 255, 255
 
+cv::Mat imageIn;
+cv::Mat initilImage;
+
 //Functions Prototypes. 
 static void clickCallback(int event, int x, int y, int flags, void* userdata);
 
@@ -25,15 +28,22 @@ static void clickCallback(int event, int x, int y, int flags, void* userdata)
 {
     if(event == (cv::EVENT_LBUTTONDOWN ))
     {
+        if (tools == 1)
+        {
+            
+        }
         
         //std::cout << "LEFT CLICK (" << x << ", " << y << ")" << std::endl;
     }
     else if (event ==cv::EVENT_LBUTTONDBLCLK)
     {
         std::cout<<"LEFT DOUBLE CLICK "<<x <<" ," << y <<std::endl;
+        imageIn = initilImage.clone();
     }
     else if(event == cv::EVENT_RBUTTONDOWN)
     {
+        //In each right click it will select tools that will be used
+        //in open CV
         std::cout << color[tools] <<" Selected" << std::endl;
         if (tools < 3)
         {
@@ -44,6 +54,7 @@ static void clickCallback(int event, int x, int y, int flags, void* userdata)
             tools = 0;
         }
     }
+
     else if(event == cv::EVENT_MBUTTONDOWN)
     {
        // std::cout << "MIDDLE CLICK (" << x << ", " << y << ")" << std::endl;
@@ -68,9 +79,8 @@ int main(int argc, char **argv)
     }
     else
     {
-        cv::Mat imageIn;
         imageIn = cv::imread(argv[1], cv::IMREAD_COLOR);
-        
+
         // check for file error
         if(!imageIn.data)
         {
@@ -79,6 +89,9 @@ int main(int argc, char **argv)
         }
         else
         {
+            //making clone of original image to reset the changes that we make 
+            initilImage = imageIn.clone();
+
             cv::imshow(DISPLAY_WINDOW_NAME, imageIn);
            
 
