3a915d8c9d89a39dac568e6df1e739e0e541dd70
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-06-24 19:17:23 -0500

    implemented double clicke

--- Assignment_1/cv_hw_1.cpp
+++ Assignment_1/cv_hw_1.cpp
@@ -2,6 +2,21 @@
 #include <string>
 #include "opencv2/opencv.hpp"
 
+
+// configuration parameters
+#define NUM_COMNMAND_LINE_ARGUMENTS 1
+#define DISPLAY_WINDOW_NAME "Assignment 1"
+
+//Global variables 
+int No_LB_Clicked = 0;        
+int No_RB_Clicked = 0;
+int Coordnate_LB_Down[2];
+int Coordinate_LB_Up[2];
+int tools = 0;
+std::string color[4] = {"EYEDROPER","CROP","PENCIL","PAINT BUCKET"};
+
+int EYEDROPER[3] ={255, 255, 255}; //intializing eye dropper to 255, 255, 255
+
 //Functions Prototypes. 
 static void clickCallback(int event, int x, int y, int flags, void* userdata);
 
@@ -11,37 +26,40 @@ static void clickCallback(int event, int x, int y, int flags, void* userdata)
     if(event == (cv::EVENT_LBUTTONDOWN ))
     {
         
-        std::cout << "LEFT CLICK (" << x << ", " << y << ")" << std::endl;
+        //std::cout << "LEFT CLICK (" << x << ", " << y << ")" << std::endl;
+    }
+    else if (event ==cv::EVENT_LBUTTONDBLCLK)
+    {
+        std::cout<<"LEFT DOUBLE CLICK "<<x <<" ," << y <<std::endl;
     }
     else if(event == cv::EVENT_RBUTTONDOWN)
     {
-        std::cout << "RIGHT CLICK (" << x << ", " << y << ")" << std::endl;
+        std::cout << color[tools] <<" Selected" << std::endl;
+        if (tools < 3)
+        {
+            tools++;
+        }
+        else
+        {
+            tools = 0;
+        }
     }
     else if(event == cv::EVENT_MBUTTONDOWN)
     {
-        std::cout << "MIDDLE CLICK (" << x << ", " << y << ")" << std::endl;
+       // std::cout << "MIDDLE CLICK (" << x << ", " << y << ")" << std::endl;
     }
     else if(event == cv::EVENT_MOUSEMOVE)
     {
-        std::cout << "MOUSE OVER (" << x << ", " << y << ")" << std::endl;
+       // std::cout << "MOUSE OVER (" << x << ", " << y << ")" << std::endl;
     }
     else if (event == cv::EVENT_LBUTTONUP)
     {
-        std::cout<<"LEFT CLICK RELEASE (" << x << " ,"<< y << ")" <<std::endl;
+       // std::cout<<"LEFT CLICK RELEASE (" << x << " ,"<< y << ")" <<std::endl;
     }
 }
 
-
-
-// configuration parameters
-#define NUM_COMNMAND_LINE_ARGUMENTS 1
-#define DISPLAY_WINDOW_NAME "Assignment 1"
-
 int main(int argc, char **argv)
 {
-    //intializing eye dropper to 255, 255, 255
-
-    int EYEDROPER[3] ={255, 255, 255}; 
     // validate and parse the command line arguments
     if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
     {
