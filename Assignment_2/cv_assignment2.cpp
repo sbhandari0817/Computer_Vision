693277daa2e549578f0c4fefacbfd2f8d6953222
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-07-11 18:27:32 -0500

    updates

---
+++ Assignment_2/cv_assignment2.cpp
@@ -0,0 +1,123 @@
+
+// include necessary dependencies
+#include <iostream>
+#include <string>
+#include "opencv2/opencv.hpp"
+
+// configuration parameters
+#define NUM_COMNMAND_LINE_ARGUMENTS 1
+
+
+int main(int argc, char **argv)
+{
+    cv::Mat imageIn;
+
+    // validate and parse the command line arguments
+    if(argc != NUM_COMNMAND_LINE_ARGUMENTS + 1)
+    {
+        std::printf("USAGE: %s <image_path> \n", argv[0]);
+        return 0;
+    }
+    else
+    {
+        imageIn = cv::imread(argv[1], cv::IMREAD_COLOR);
+
+        // check for file error
+        if(!imageIn.data)
+        {
+            std::cout << "Error while opening file " << argv[1] << std::endl;
+            return 0;
+        }
+    }
+
+    // get the image size
+    std::cout << "image width: " << imageIn.size().width << std::endl;
+    std::cout << "image height: " << imageIn.size().height << std::endl;
+    std::cout << "image channels: " << imageIn.channels() << std::endl;
+
+
+    //Resize the image 
+    cv::Mat imageResiged;
+    cv::resize(imageIn, imageResiged, cv::Size(imageIn.cols /2, imageIn.rows /2));
+
+    //changing original image to resiged. 
+    imageIn = imageResiged.clone();
+    // convert the image to grayscale
+    cv::Mat imageGray;
+    cv::cvtColor(imageIn, imageGray, cv::COLOR_BGR2GRAY);
+
+    // find the image edges
+    cv::Mat imageEdges;
+    const double cannyThreshold1 = 100;
+    const double cannyThreshold2 = 200;
+    const int cannyAperture = 3;
+    cv::Canny(imageGray, imageEdges, cannyThreshold1, cannyThreshold2, cannyAperture);
+
+    // locate the image contours (after applying a threshold or canny)
+    std::vector<std::vector<cv::Point> > contours;
+    cv::findContours(imageEdges, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+    // draw the contours
+    cv::Mat imageContours = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    cv::RNG rand(12345);
+    for(int i = 0; i < contours.size(); i++)
+    {
+        cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+        cv::drawContours(imageContours, contours, i, color);
+    }
+
+    // compute minimum area bounding rectangles
+    std::vector<cv::RotatedRect> minAreaRectangles(contours.size());
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // compute a minimum area bounding rectangle for the contour
+        minAreaRectangles[i] = cv::minAreaRect(contours[i]);
+    }
+
+    // draw the rectangles
+    cv::Mat imageRectangles = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    for(int i = 0; i < contours.size(); i++)
+    {
+        cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+        cv::Point2f rectanglePoints[4];
+        minAreaRectangles[i].points(rectanglePoints);
+        for(int j = 0; j < 4; j++)
+        {
+            cv::line(imageRectangles, rectanglePoints[j], rectanglePoints[(j+1) % 4], color);
+        }
+    }
+
+    // fit ellipses to contours containing sufficient inliers
+    std::vector<cv::RotatedRect> fittedEllipses(contours.size());
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // compute an ellipse only if the contour has more than 5 points (the minimum for ellipse fitting)
+        if(contours.at(i).size() > 5)
+        {
+            fittedEllipses[i] = cv::fitEllipse(contours[i]);
+        }
+    }
+
+    // draw the ellipses
+    cv::Mat imageEllipse = cv::Mat::zeros(imageEdges.size(), CV_8UC3);
+    const int minEllipseInliers = 500;
+    for(int i = 0; i < contours.size(); i++)
+    {
+        // draw any ellipse with sufficient inliers
+        if(contours.at(i).size() > minEllipseInliers)
+        {
+            cv::Scalar color = cv::Scalar(rand.uniform(0, 256), rand.uniform(0,256), rand.uniform(0,256));
+            cv::ellipse(imageEllipse, fittedEllipses[i], color, 2);
+            cv::ellipse(imageIn, fittedEllipses[i],color,2);
+        }
+    }
+
+    // display the images
+    cv::imshow("imageIn", imageIn);
+    // cv::imshow("imageGray", imageGray);
+    // cv::imshow("imageEdges", imageEdges);
+    // cv::imshow("imageContours", imageContours);
+    // cv::imshow("imageRectangles", imageRectangles);
+    // cv::imshow("imageEllipse", imageEllipse);
+    cv::waitKey();
+}
