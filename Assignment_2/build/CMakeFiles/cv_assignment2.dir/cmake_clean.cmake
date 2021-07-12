24098aa21e5138df753386b343ab9cdc0f488a35
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-07-11 23:56:48 -0500

    ass 2

---
+++ Assignment_2/build/CMakeFiles/cv_assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_assignment2.dir/cv_assignment2.cpp.o"
+  "cv_assignment2"
+  "cv_assignment2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
